9ea6ea30c6e503287aaf4cbbf78e58bd04582403
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2024-10-08 11:01:26 +0600

    Login done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from './dto/user.dto';
 import { UserResponseType } from './types/userResponse.type';
 import { UserService } from './user.service';
@@ -12,6 +13,12 @@ export class UserController {
     @Body() createUserDto: CreateUserDto,
   ): Promise<UserResponseType> {
     const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  async login(@Body() loginDto: LoginDto): Promise<UserResponseType> {
+    const user = await this.userService.loginUser(loginDto);
     return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { compare } from 'bcrypt';
 import { Model } from 'mongoose';
+import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from './dto/user.dto';
 import { UserResponseType } from './types/userResponse.type';
 import { UserEntity } from './user.entity';
@@ -23,6 +25,29 @@ export class UserService {
 
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
+  }
+
+  async loginUser(loginDto: LoginDto): Promise<UserEntity> {
+    const user = await this.userModel
+      .findOne({ email: loginDto.email })
+      .select('+password');
+
+    if (!user) {
+      throw new HttpException(
+        'User not found!',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const isPasswordCorrect = await compare(loginDto.password, user.password);
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        'Incorrect password!',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    return user;
   }
 
   buildUserResponse(userEntity: UserEntity): UserResponseType {
